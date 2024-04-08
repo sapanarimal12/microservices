b1ee3045124ed138a5557011369832cafdee5540
sapanarimal12 <rimalsapanaaa55@gmail.com>
2024-04-08 13:26:14 +0545

    implemented api gateway config server and client

---
+++ Config-Server/src/test/java/com/configServer/Config/Server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configServer.Config.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
